c3ad284d81181a2ce975c05ed745fb02cd792ee9
TetsuoMiyake <tmky1919mnstwebcamp@gmail.com>
2021-08-14 12:40:13 +0000

    commit-4-finish

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '039ade76f9f661641be426458c1b6c776f52419dcfb990e38b13cb44da1156f3a7ce24af717e38515775a0c35e7b2e68fc44bf436816326593319dda50ce8166'
